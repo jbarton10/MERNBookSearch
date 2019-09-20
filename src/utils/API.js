ed3c66ddf52adc195da6a63b31bdf6363f09bc22
Jamie Barton <jbarton10@email.arizona.edu>
2019-09-19 18:50:21 -0700

    Added more things serverJS ect

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,10 +1,10 @@
+const axios = require("axios");
+const router = require("express").Router();
 
-import axios from "axios";
-
-// The getRecipes method retrieves recipes from the server
+// The getBooks method retrieves books from the server
 // It accepts a "query" or term to search the recipe api for
 export default {
-  getRecipes: function(query) {
+  getBooks: function(query) {
       //Books should go here
     return axios.get("/api/", { params: { q: query } });
   }
