993c45e1100175b2469a298e4ca5667f54607ba2
Jamie Barton <jbarton10@email.arizona.edu>
2019-09-23 22:10:02 -0700

    No idea whats going on

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import axios from "axios";
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+class App extends React.Component {
+
+  callDB = () => {
+    axios.get("http://localhost:3001/books").then(function(res){
+      console.log(res.data);
+    })
+  }
+
+  componentDidMount(){
+    this.callDB();
+  }
+  render(){
+    return<div></div>
+  }
+} 
 
 export default App;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
-const routes = require("./routes");
+//const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -21,6 +21,11 @@ const PORT = process.env.PORT || 3001;
 // // Connect to the Mongo DB
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
+
+
+app.get("/books",function(req, res){
+  res.json({author: "JKROWLING"});
+})
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
